e5f6ff1ed7de3cf0d89a63cdc3f1ea6472105459
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-07-13 18:04:13 +0900

    型修正

--- fastapi_memoapp/models/memo.py
+++ fastapi_memoapp/models/memo.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, func
 from db import Base
 from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
 
 class Memo(Base):
     __tablename__ = "memos"
     memo_id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(50), nullable=False)
-    description = Column(String(255), nullable=True)
+    title: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
     created_at = Column(DateTime, default=datetime.now())
-    updated_at = Column(DateTime)
