d84b028c83d621e16c22759cd6485d462c244df4
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-07-13 16:54:05 +0900

    init db

---
+++ fastapi_memoapp/db.py
@@ -0,0 +1,19 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+DATABASE_URL = 'sqlite+aiosqlite:///' + os.path.join(base_dir, 'memodb.sqlite')
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
+
+async def get_dbsession():
+    async with async_session() as session:
+        yield session
